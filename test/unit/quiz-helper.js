2d4585327748625120e8d0233b70fb19550b670b
hideokamoto <hide.okamoto@digitalcube.jp>
2018-03-08 02:41:20 +0900

    quiz answer has been random

--- test/unit/quiz-helper.js
+++ test/unit/quiz-helper.js
@@ -3,7 +3,11 @@ const assert = require('power-assert')
 const helpers = require('./intent/helpers')
 // const states = require('../../lib/states')
 const getLocaleFromEvent = require('../../lib/utils/getLocale')
-const { getQuestions, getServiceNames } = require('../../lib/utils/quiz')
+const {
+  getQuestions,
+  getServiceNames,
+  makeQuiz
+} = require('../../lib/utils/quiz')
 const { event } = helpers
 
 describe('Quiz Helper', () => {
@@ -24,10 +28,23 @@ describe('Quiz Helper', () => {
       assert.notEqual(Object.keys(result).length, 0)
     })
   })
-  it('#getServiceNames()', () => {
+  describe('#getServiceNames()', () => {
     it('should get one questions at least', () => {
       const result = getServiceNames('ja-JP')
       assert.notEqual(result.length, 0)
+    })
+  })
+  describe('#makeQuiz()', () => {
+    const services = getQuestions('ja-JP')
+    const serviceNames = getServiceNames('ja-JP')
+    it('should match answer number and selected number', () => {
+      const answerNumber = Math.floor(Math.random() * 3) + 1
+      const { selection, correctServiceName } = makeQuiz(
+        answerNumber,
+        serviceNames,
+        services
+      )
+      assert.equal(correctServiceName, selection[answerNumber - 1])
     })
   })
 })
